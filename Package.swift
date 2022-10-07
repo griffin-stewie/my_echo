c19fa587c00896ab57ff1911d9238685e3301041
griffin-stewie <panterathefamilyguy@gmail.com>
2022-10-07 23:03:47 +0900

    fix warning

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "my_echo",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
