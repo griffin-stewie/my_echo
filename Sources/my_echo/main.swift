7967af68bff082b690191cb31d904f62b67c45d2
griffin-stewie <panterathefamilyguy@gmail.com>
2022-10-08 11:56:36 +0900

    bump version

--- Sources/my_echo/main.swift
+++ Sources/my_echo/main.swift
@@ -4,7 +4,7 @@ struct RootCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "my_echo",
         abstract: "echo",
-        version: "0.0.14"
+        version: "0.0.15"
     )
 
     @OptionGroup()
