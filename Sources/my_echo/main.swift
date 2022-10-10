3af6b5584a55839321b5fba1f25a56937c6f7c48
griffin-stewie <panterathefamilyguy@gmail.com>
2022-10-10 16:45:59 +0900

    bump minor version

--- Sources/my_echo/main.swift
+++ Sources/my_echo/main.swift
@@ -4,7 +4,7 @@ struct RootCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "my_echo",
         abstract: "echo",
-        version: "0.0.18"
+        version: "0.1.0"
     )
 
     @OptionGroup()
